6f53319222090eb7084f05fa93d4097d85f3c15b
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2024-11-08 15:38:10 -0300

    Initial commit

---
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -0,0 +1,53 @@
+package br.com.alura.adopet.api.validacoes;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Incubating;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ValidacaoPetDisponivelTest {
+
+    @InjectMocks
+    ValidacaoPetDisponivel validacaoPetDisponivel;
+
+    @Mock
+    private PetRepository petRepository;
+
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
+    @Test
+    void deveriaPermitirSolicitacaoDeAdocaoPet(){
+
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+        Assertions.assertDoesNotThrow(() -> validacaoPetDisponivel.validar(dto));
+    }
+
+    @Test
+    void naoDeveriaPermitirSolicitacaoDeAdocaoPet(){
+
+
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+        Assertions.assertThrows(ValidacaoException.class ,() -> validacaoPetDisponivel.validar(dto));
+    }
+
+}
