a253abcafd5ba6b562d1a55f382e83d2cffaf419
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2024-11-12 13:00:28 -0300

    Criação de teste de service

---
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,65 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.then;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+
+
+        @InjectMocks
+        private AdocaoService service;
+
+        @Mock
+        private AdocaoRepository repository;
+
+        @Mock
+        private PetRepository petRepository;
+
+        @Mock
+        private TutorRepository tutorRepository;
+
+        @Mock
+        private EmailService emailService;
+
+        @Mock
+        private List<ValidacaoSolicitacaoAdocao> validacoes;
+
+        @Mock
+        private Pet pet;
+
+        @Mock
+        private Tutor tutor;
+
+        @Mock
+        private Abrigo abrigo;
+
+        private SolicitacaoAdocaoDto dto;
+
+
+        void deveriaSalvarAdocaoAoSolicitar() {
+
+            service.solicitar(dto);
+
+            then(petRepository).should().save(any());
+        }
+
+}
