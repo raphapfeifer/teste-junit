3ef735c4990d469896815875393e4623a1354b1c
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2024-11-25 04:23:38 -0300

    Testes na controller

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,22 +1,29 @@
 package br.com.alura.adopet.api.controller;
 
+
+import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@SpringBootTest
+@SpringBootTest(classes = AdocaoController.class)
 @AutoConfigureMockMvc
 class AdocaoControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @MockBean
+    private AdocaoService service;
 
     @Test
     void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
@@ -29,10 +36,34 @@ class AdocaoControllerTest {
                 post("/adocoes")
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
         ).andReturn().getResponse();
 
         //ASSERT
         Assertions.assertEquals(400,response.getStatus());
+    }
+
+    @Test
+    void deveriaDevolverCodigo200ParaSolicitacaoDeAdocaoSemErros() throws Exception {
+
+        //ARRANGE
+        String json = """
+                    {
+                        "idPet": 1,
+                        "idTutor": 1,
+                        "motivo": "Motivo qualquer"
+                    }
+                """;
+
+        //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(json)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //ASSERT
+        Assertions.assertEquals(200,response.getStatus());
     }
 
 }
