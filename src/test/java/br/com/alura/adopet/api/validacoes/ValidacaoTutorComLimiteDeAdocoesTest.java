22b42d62a195001dceb40d057cb31bd7ae9786f2
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2024-12-09 08:19:47 -0300

    Correção no teste de validação de tutor com limite de adoções

--- src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoesTest.java
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComLimiteDeAdocoesTest.java
@@ -2,18 +2,28 @@ package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +39,18 @@ class ValidacaoTutorComLimiteDeAdocoesTest {
     private TutorRepository tutorRepository;
 
     @Mock
+    private Pet pet;
+
+    @Mock
+    private Tutor tutor;
+
+    @Spy
+    private List<Adocao> listaDeAdocoes = new ArrayList<>();
+
+    @Spy
+    private Adocao adocao;
+
+    @Mock
     private SolicitacaoAdocaoDto dto;
 
 
@@ -36,7 +58,14 @@ class ValidacaoTutorComLimiteDeAdocoesTest {
     @Test
     void naoDevePermitirSolicitacaoDeAdocaoTutorAtingiuLimite5Adocoes(){
 
-        given(adocaoRepository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO)).willReturn(5);
+
+        adocao = new Adocao(tutor, pet, "Motivo qualquer");
+        adocao.marcarComoAprovada();
+        for(int i = 0; i < 5; i++) {
+            listaDeAdocoes.add(adocao);
+        }
+        BDDMockito.given(adocaoRepository.findAll()).willReturn(listaDeAdocoes);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
 
         assertThrows(ValidacaoException.class,() ->validador.validar(dto));
 
@@ -45,7 +74,11 @@ class ValidacaoTutorComLimiteDeAdocoesTest {
     @Test
     void devePermitirSolicitacaoDeAdocaoTutorNaoAtingiuLimite5Adocoes(){
 
-        given(adocaoRepository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO)).willReturn(4);
+        adocao = new Adocao(tutor, pet, "Motivo qualquer");
+        adocao.marcarComoAprovada();
+        listaDeAdocoes.add(adocao);
+        BDDMockito.given(adocaoRepository.findAll()).willReturn(listaDeAdocoes);
+        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
 
         assertDoesNotThrow(() ->validador.validar(dto));
 
